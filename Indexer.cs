05e3ede8758b6799c844dace6723fb17c8c94901
Sergei <sergey_zyuzev@hotmail.com>
2018-01-16 20:41:01 +0300

    Pass tests

--- Indexer.cs
+++ Indexer.cs
@@ -6,55 +6,31 @@ using System.Threading.Tasks;
 
 namespace Incapsulation.Weights
 {
-	public class Indexer
+    public class Indexer
     {
-        public readonly double [] array;
+        double[] array;
         int start;
-        public int Start
-        {
-            get { return start; }
-            set
-            {
-                if (value < 0) throw new ArgumentException();
-                if (value >= array.Length) throw new ArgumentException();
-                start = value;
-            }
-        }
-
         int length;
         public int Length
         {
             get { return length; }
-            set
-            {
-                if (value < 0) throw new ArgumentException();
-                if (value >= length) throw new ArgumentException();
-                length = value;
-            }
         }
 
-
-
-        public Indexer(double [] array, int start, int length)
+        public Indexer(double[] array, int start, int length)
         {
+            if (start < 0 || start >= array.Length) throw new ArgumentException();
+            this.start = start;
+            if (length < start || length > array.Length) throw new ArgumentException();
+            this.length = length;
             this.array = array.Skip(start).Take(length).ToArray();
-            //this.array = array;
-            Start = start;
-            Length = length;
         }
 
         public double this[int index]
         {
-            get
-            {
-                if ((index < 0) || (index >= Length)) throw new ArgumentException();
-                return array[index];
-            }
-            set
-            {
-                if ((index < 0) || (index >= Length)) throw new ArgumentException();
-                array[index] = value;
-            }
+            get { return array[index]; }
+            set { array[index] = value; }
         }
     }
+
+   
 }
